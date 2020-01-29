16d1fda96cfe31f55b30a14ab89bb037ffc3d6d4
Eric Labelle <elabelle@genetec.com>
2020-01-29 16:15:01 -0500

    Initial commit for extension files

---
+++ PublishTestResultScreenshotV0/index.ts
@@ -0,0 +1,124 @@
+// **********************************************************************************
+// ************************* FOR LOCAL USE ******************************************
+// process.env.ENDPOINT_AUTH_PARAMETER_SYSTEMVSSCONNECTION_ACCESSTOKEN=""
+// process.env.SYSTEM_TEAMPROJECT = ""
+// process.env.INPUT_ORGANIZATION = ""
+// process.env.INPUT_SCREENSHOTFOLDER = ""
+// process.env.BUILD_BUILDID = ""
+// **********************************************************************************
+
+import * as tl from "azure-pipelines-task-lib/task"
+import * as azdev from "azure-devops-node-api";
+import * as ta from "azure-devops-node-api/TestApi";
+import fs from "fs";
+import { TestOutcome, ShallowTestCaseResult, TestAttachmentRequestModel, TestAttachmentReference } from 'azure-devops-node-api/interfaces/TestInterfaces';
+
+const DEFAULT_SCREENSHOT_FOLDER = "./app/build/reports/androidTests/connected/screenshots/failures/";
+const PARAM_SCREENSHOT_FOLDER = "screenshotFolder";
+const PARAM_ORGANIZATION = "organization";
+
+let project = tl.getVariable("System.TeamProject");
+let testApi: ta.ITestApi
+
+async function run() {
+    try {
+        let authToken = tl.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false);
+        let authHandler = azdev.getPersonalAccessTokenHandler(authToken);
+        let connection = new azdev.WebApi("https://dev.azure.com/" + getOrganization(), authHandler);
+        testApi = await connection.getTestApi();
+        await testApi.getTestResultsByBuild(project, +tl.getVariable("Build.BuildId"), undefined, [TestOutcome.Failed])
+            .then(async failedTests =>  uploadScreenshots(failedTests))
+            .catch(err => tl.setResult(tl.TaskResult.Failed, err.message))
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
+
+async function uploadScreenshots(failedTests: ShallowTestCaseResult[]) {
+    let apiCalls: Promise<any>[] = [];
+    let missingScreenshots: Error[] = [];
+    let totalFailures = failedTests.length
+
+    if(totalFailures <= 0) {
+        tl.setResult(tl.TaskResult.Skipped, "No test failures found.")
+        return
+    }
+    console.log(totalFailures + " tests failed. Will proceed with screenshot upload.")
+    failedTests.forEach(async (failedTest: ShallowTestCaseResult, index) => {
+        let testName = failedTest.automatedTestName;
+        let className = failedTest.automatedTestStorage;
+        let imgPath = getScreenshotFolder() + className + "/" + testName + ".png";//TODO make it configurable in upcoming version
+        tl.debug("Searching for image at path: " + imgPath);
+        if (fs.existsSync(imgPath)) {
+            let imageAsBase64 = fs.readFileSync(imgPath, 'base64');
+            let attachment: TestAttachmentRequestModel = {fileName: testName + ".png", stream: imageAsBase64};
+            
+            apiCalls.push(testApi.createTestResultAttachment(attachment, project, failedTest.runId!, failedTest.id!));
+        } else {
+            tl.debug("Failure - No screenshot found for " + className + "/" + testName);
+            missingScreenshots.push(Error("No screenshot found for " + className + "/" + testName));
+        }
+    });
+    Promise.all(apiCalls).then(function(attachmentResults) {
+        let attachmentFailedCount = attachmentResults.filter(attachmentResult => attachmentResult == null).length
+        let hasMissingScreenshot = missingScreenshots.length > 0
+        let hasAttachmentFailure = attachmentFailedCount > 0
+
+        tl.debug("hasMissingScreenshot: " + hasMissingScreenshot + " -- hasAttachmentFailure: " + hasAttachmentFailure)
+        if (hasMissingScreenshot || hasAttachmentFailure) {
+            let message = ""
+            if (hasMissingScreenshot) message += (totalFailures != missingScreenshots.length ? "Some screenshots were missing. " : "All screenshots were missing. ");
+            if (hasAttachmentFailure) message += (totalFailures != attachmentFailedCount) ? "Some attachments failed. " : "All attachments failed. "
+
+            tl.setResult(tl.TaskResult.SucceededWithIssues, message);
+        } else {
+            attachmentResults.forEach(attachmentResult => tl.debug("attachment success-> " + (attachmentResult as TestAttachmentReference).url))
+            tl.setResult(tl.TaskResult.Succeeded, "All screenshots were published successfully");
+        }
+        console.log("Task completed. Published " + (attachmentResults.length - attachmentFailedCount) + "/" + totalFailures + " screenshots")
+    })
+}
+
+/**
+ * Get the input parameter "screenshotFolder"
+ * 
+ * @returns the value from the input param or DEFAULT_SCREENSHOT_FOLDER
+ */
+function getScreenshotFolder(): string {
+    let screenshotFolder = tl.getInput(PARAM_SCREENSHOT_FOLDER)
+    if (isNullEmptyOrUndefined(screenshotFolder)) {
+        return DEFAULT_SCREENSHOT_FOLDER
+    } else {
+        return screenshotFolder += screenshotFolder.endsWith("/") ? "" : "/"
+    }
+}
+
+/**
+ * Get the input parameter "organization" in order to make REST calls
+ * 
+ * **NOTE**: this is needed until a System.OrganizationName is exposed (*see: https://developercommunity.visualstudio.com/idea/747962/add-a-variable-to-access-organization-name.html*)
+ * 
+ * @returns the organization
+ * @throws an error if no value was given
+ */
+function getOrganization(): string {
+    let organization = tl.getInput(PARAM_ORGANIZATION)
+    if (isNullEmptyOrUndefined(organization)) {
+        throw Error("Organization is mandatory")
+    } else {
+        return organization
+    }
+}
+
+/**
+ * Test the given parameter to see if it's usable.
+ * 
+ * @param obj the obj to test
+ * @returns true if the param is neither either null, empty, or undefined
+ */
+function isNullEmptyOrUndefined(obj: any): boolean {
+    return obj === null || obj === '' || obj === undefined
+}
---
+++ README.md
@@ -0,0 +1,108 @@
+# PublishTestResultScreenshot for Azure DevOps
+Get more insight on your UI test failure with screenshots. 
+
+## Installation
+You can get the task from the [Visual Studio MarketPlace](https://marketplace.visualstudio.com/items?itemName=Genetec.publish-test-result-screenshot).
+
+## How to use
+### Requirements
+* Capture your screenshot on test failure using the following path `automatedTestStorage/automatedTestName` (*see [json response](https://docs.microsoft.com/en-us/rest/api/azure/devops/test/results/get?view=azure-devops-rest-5.1#examples) example*)
+  * Currently only support `.png` images
+* The task must be placed **after** the [PublishTestResult](https://docs.microsoft.com/en-us/azure/devops/pipelines/tasks/test/publish-test-results) task in your pipeline
+* If you enabled *Fail build on test failure* on **PublishTestResult** task preceding **PublishTestResultScreenshot**, you will need to also enable the *Continue on error* to make sure **PublishTestResultScreenshot** is not skipped.
+
+### Compatibility
+Version 0.1.x is compatible with:
+* Azure DevOps Service Cloud
+* Test type: JUnit
+* Microsoft-hosted agents: macOS 10.14 & 10.13, ubuntu16.04, win2019
+> **NOTE** that emulators are currently only available on macOS agents, so for Android UI Testing you will be limited to 10.14 or 10.13, but the task is not dependent on macOS agents.
+
+### Configure
+#### Available inputs:
+Required
+* `organization`: specify your organizationName (*can be found from devops instance url http://dev.azure.com/{yourOrg}*)
+
+Optional
+* `screenshotFolder`: you can change where the task looks for screenshot, defaults to "./app/build/reports/androidTests/connected/screenshots/failures/"
+
+#### Using classic editor
+<img src="images/task-config-preview.png" width="600" />
+
+#### Using YAML
+```yaml
+- task: Genetec.publish-test-result-screenshot.pipeline-extension.PublishTestResultScreenshot@0
+  displayName: 'Publish test result screenshot'
+  inputs:
+    organization: {yourOrganizationName}
+    screenshotFolder: {yourCustomPath}
+```
+## Specifications
+This task uses [azure-devops-node-api](https://github.com/microsoft/azure-devops-node-api) which, under the hood, uses the [Azure DevOps REST Api](https://docs.microsoft.com/en-us/rest/api/azure/devops/).
+
+### Task Results
+* `Succeeded` - All the screenshots were successfully uploaded
+* `SucceededWithIssues` (2 self explained possibles messages)
+  * Some/All screenshots upload failed AND/OR
+  * Some/All screenshots files were missing
+* `Skipped` - No test failures were found
+* `Error` - Usually related to authentification, connection, networkError etc.
+
+## How to build
+### Prerequisites
+You will need to install nvm, and npm with node to run this project.
+
+* Install [nvm](https://github.com/nvm-sh/nvm#installation-and-update)
+* Install node (npm is bundled)
+  * `nvm install node`
+  * `nvm use node`
+* Install Typescript
+  * `npm install typescript -g`
+
+
+#### Verify versions
+I used the latest version but just make sure that all commands return a version number.
+* `nvm --version`
+* `npm --version`
+* `node --version`
+
+### Getting started
+
+#### Clone the repo
+`git clone git@github.com:Genetec/azure-devops-extension-publishtestresultscreenshot.git`
+
+#### Setup for local use
+1. Uncomment the header of [index.ts](./PublishTestResultScreenshotV0/index.ts) and set input values
+```
+// **********************************************************************************
+// ************************* FOR LOCAL USE ******************************************
+// process.env.ENDPOINT_AUTH_PARAMETER_SYSTEMVSSCONNECTION_ACCESSTOKEN = ""
+// process.env.SYSTEM_TEAMPROJECT = ""
+// process.env.INPUT_ORGANIZATION = ""
+// process.env.INPUT_SCREENSHOTFOLDER = ""
+// process.env.BUILD_BUILDID = ""
+// **********************************************************************************
+```
+2. Assign your access token to `ENDPOINT_AUTH_PARAMETER_SYSTEMVSSCONNECTION_ACCESSTOKEN`
+
+#### Compile the typescript into javascript
+* `cd PublishTestResultScreenshotV0`
+* `npm install`
+* `tsc`
+
+#### Execute the task locally 
+* `node index.js`
+
+#### Deploy and test on DevOps
+Before submitting your PR, make sure to test your changes on your DevOps instance using the following command:
+```
+tfx build tasks upload --overwrite --task-path "myPathToTheTask" --service-url "https://myOrg.visualstudio.com/DefaultCollection" --auth-type pat -t "xxx"
+```
+> The PAT (*personal access token*) requires only the **Manage Agent Pool** permission.
+
+## How to contribute
+Check [Contribution Guide](./CONTRIBUTING.md)
+
+## License
+Published under [Apache 2.0](./LICENSE) License
+
